0bcbb61ac646067da287607ba3990555252421c1
Somya Dwivedi <somyadwivedi21@gmail.com>
2019-06-20 06:41:29 +0000

    improve variable names

--- card_game.py
+++ card_game.py
@@ -1,46 +1,41 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import random
+
 computer =[i for i in range(2,15)]
 human =[i for i in range(2,15)]
 diamond =[i for i in range(2,15)]
 
 def check_winner():
-    total_score1 = 0
-    total_score2 = 0
+    human_score = 0
+    computer_score = 0
     while len(diamond) != 0:
         
         diamond_card = random.choice(diamond)
         print('bid for {}'.format(diamond_card) )
-        move1 = int(input("enter your bid"))
-        if move1 not in human:
+        human_throw = int(input("enter your bid"))
+        if human_throw not in human:
             print("game over ..you have already played this card")
             break
-        move2 = random.choice(computer) 
-        print('computer move {}'.format(move2) )
-        score1,score2 = compare_score(move1,move2,diamond_card)
+        computer_throw = random.choice(computer) 
+        print('computer move {}'.format(computer_throw) )
+        score1,score2 = compare_score(human_throw,computer_throw,diamond_card)
         print('score_for_this_round {} {}'.format(score1,score2))
-        total_score1 = total_score1 + score1
+        human_score = human_score + score1
         
-        total_score2 = total_score2 + score2
-        print(total_score1)
-        print(total_score2)
-        human.remove(move1)
-        computer.remove(move2)
-    return total_score1,total_score2
+        computer_score = computer_score + score2
+        print(human_score)
+        print(computer_score)
+        human.remove(human_throw)
+        computer.remove(computer_throw)
+    return human_score,computer_score
 
-def compare_score(move1,move2,diamond_card):
+def compare_score(human_throw, computer_throw, diamond_card):
     d_card = diamond_card
     diamond.remove(diamond_card)
-    if move1 > move2:
+    if human_throw > computer_throw:
         return d_card,0
-    if move1 < move2:
+    if human_throw < computer_throw:
         return 0,d_card
-    if move1  == move2:
+    if human_throw  == computer_throw:
         return d_card/2,d_card/2
         
         
