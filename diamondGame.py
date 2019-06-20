e87b5dbbece452c3af9171769d1e01ee31e17778
Divya Gahlot <gahlotdivya1019@gmail.com>
2019-06-20 07:11:05 +0000

    Update diamondGame.py

--- diamondGame.py
+++ diamondGame.py
@@ -5,8 +5,9 @@ def getThrow(H_DECK, C_DECK, REFERENCE_DECK, H_SCORE, C_SCORE):
     REFERENCE_THROW = random.choice(REFERENCE_DECK)
     print("THE DIAMOND CARD IS: " + str(REFERENCE_THROW))
     H_THROW = int(input("Enter your Bid :- "))
-    '''if (H_THROW not in H_DECK):
-        return "You have already played this card!" '''
+    if (H_THROW not in H_DECK):
+        print( "You have already played this card!")
+        exit (0)
     C_THROW = random.choice(C_DECK)
     print("Computer: " + str(C_THROW))
     H_THROW, C_THROW, REFERENCE_THROW, H_SCORE, C_SCORE = gamePlay(H_THROW, C_THROW, REFERENCE_THROW, H_SCORE, C_SCORE)
