dd79399cd0e1cd4568681590cdcddfa057daa34c
Divya Gahlot <gahlotdivya1019@gmail.com>
2019-06-20 07:06:40 +0000

    without strategy running code

--- diamondGame.py
+++ diamondGame.py
@@ -1,37 +1,45 @@
 import random
 
-H_SCORE = 0
-C_SCORE = 0
-
-REFERENCE_DECK = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-H_DECK = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-C_DECK = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-
-def getThrow(H_DECK, C_DECK, REFERENCE_DECK):
+def getThrow(H_DECK, C_DECK, REFERENCE_DECK, H_SCORE, C_SCORE):
     
-    REFERENCE_CARD = random.choice(REFERENCE_DECK)
+    REFERENCE_THROW = random.choice(REFERENCE_DECK)
+    print("THE DIAMOND CARD IS: " + str(REFERENCE_THROW))
     H_THROW = int(input("Enter your Bid :- "))
+    '''if (H_THROW not in H_DECK):
+        return "You have already played this card!" '''
     C_THROW = random.choice(C_DECK)
+    print("Computer: " + str(C_THROW))
+    H_THROW, C_THROW, REFERENCE_THROW, H_SCORE, C_SCORE = gamePlay(H_THROW, C_THROW, REFERENCE_THROW, H_SCORE, C_SCORE)
+    return H_THROW, C_THROW, REFERENCE_THROW, H_SCORE, C_SCORE
     
-    winnerOfBid(H_THROW, C_THROW, REFERENCE_THROW)
-    
-def round(H_THROW, C_THROW, REFERENCE_THROW):
+def gamePlay(H_THROW, C_THROW, REFERENCE_THROW, H_SCORE, C_SCORE):
     if H_THROW > C_THROW:
         H_SCORE += REFERENCE_THROW
+        print("You win this round")
     elif H_THROW < C_THROW:
         C_SCORE += REFERENCE_THROW
-    elif H_THROW = C_THROW:
+        print("Computer wins this round")
+    else:
         H_SCORE += REFERENCE_THROW / 2
         C_SCORE += REFERENCE_THROW / 2
+        print("DRAW")
+    return H_THROW, C_THROW, REFERENCE_THROW, H_SCORE, C_SCORE
 
-    H_DECK.remove(H_THROW)
-    C_DECK.remove(C_THROW)
-    REFERENCE_DECK.remove(REFERENCE_THROW)
-    return H_DECK, C_DECK, REFERENCE_DECK
-    
+def checkWinner():  
+    rounds = 13
+    H_SCORE = 0
+    C_SCORE = 0
+    REFERENCE_DECK = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    H_DECK = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    C_DECK = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
-    
-getThrow(H_DECK, C_DECK, REFERENCE_DECK):  
+    for i in range(rounds):
+        H_THROW, C_THROW, REFERENCE_THROW, H_SCORE, C_SCORE = getThrow(H_DECK, C_DECK, REFERENCE_DECK, H_SCORE, C_SCORE)
+        print("Updated Score: " + str(int(H_SCORE)) + " " + str(int(C_SCORE)))
+        print("---------------------------------------------------")
+        H_DECK.remove(H_THROW)
+        C_DECK.remove(C_THROW)
+        REFERENCE_DECK.remove(REFERENCE_THROW)
+        rounds -= 1
 
-
-
+checkWinner()
